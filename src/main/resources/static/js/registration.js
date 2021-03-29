2c27679bfebe2697fbbc8a5d462f7b51df24fd1c
Evgeniy <evgeniy.zhurenko@gmail.com>
2021-03-29 09:18:30 +0300

    add js registration

--- src/main/resources/static/js/registration.js
+++ src/main/resources/static/js/registration.js
@@ -1,30 +1,28 @@
 let inputPasswordRepeat = document.getElementsByName("repeat_password")[0];
 
-console.dir(inputPasswordRepeat);
-
 inputPasswordRepeat.addEventListener("input", buttonActivate);
-
 
 function buttonActivate() {
 
     let inputPassword = document.getElementsByName("password")[0];
     let button = document.getElementById("button");
 
-    console.dir(button);
-    console.log(inputPassword.value);
-    console.log(inputPasswordRepeat.value);
-
-    console.log(inputPassword.value === inputPasswordRepeat.value);
-
-    if (inputPassword.value.length === inputPasswordRepeat.value.length){
-        if (inputPassword.value === inputPasswordRepeat.value) {
-            button.disabled = "";
-        } else {
+    if (inputPassword.value === inputPasswordRepeat.value) {
+        button.disabled = "";
+        button.classList.remove("btn_disable");
+        button.classList.add("btn" );
+        if(button.innerHTML === "Пароли не совпадают!"){
+            button.innerHTML = "Регистрация";
+        }
+    } else {
+        if(inputPassword.value.length === inputPasswordRepeat.value.length) {
             button.innerHTML = "";
             button.innerHTML = "Пароли не совпадают!";
-            button.color = "red";
-            if(button.disabled !== "disabled")
-                button.disabled = "disabled";
+            button.style.color = "red";
+            if (button.className === "btn") {
+                button.classList.remove("btn");
+                button.classList.add("btn_disable");
+            }
         }
     }
 }
