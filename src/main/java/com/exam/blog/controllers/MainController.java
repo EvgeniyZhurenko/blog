37078f788496c6af92e1ab884d0660f98d36bc3b
Evgeniy <evgeniy.zhurenko@gmail.com>
2021-07-14 23:02:03 +0300

    change CustomLogoutSuccessHandler

--- src/main/java/com/exam/blog/controllers/MainController.java
+++ src/main/java/com/exam/blog/controllers/MainController.java
@@ -15,8 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+
 
 /**
  @author Zhurenko Evgeniy
